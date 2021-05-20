dee1526e12bc2905064bebc167b702dd4a9801b7
mikangogo <mikan750r@gmail.com>
2021-05-20 12:23:33 +0900

    デプロイ関係の調査

---
+++ .ci/package.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir deploy
+mkdir deploy/Scenarios
+rsync -av --exclude 'deploy' --exclude '.git' --exclude '.*' ./ ./deploy/Scenarios
+cd deploy
+zip -q JRTrainPack.zip -r Scenarios
+cd ..
